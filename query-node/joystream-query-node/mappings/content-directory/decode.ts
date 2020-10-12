af8095f2ecaa406f6f3c01f5423d4ef9aad5c0ed
iorveth <aenseidheiorweth@gmail.com>
2020-10-12 11:49:32 +0300

    Cont_dir mappings: upgrade contentDirectory_EntityRemoved mapping

--- query-node/joystream-query-node/mappings/content-directory/decode.ts
+++ query-node/joystream-query-node/mappings/content-directory/decode.ts
@@ -2,12 +2,27 @@ import Debug from "debug";
 import * as BN from "bn.js";
 
 import { SubstrateEvent } from "../../generated/indexer";
-import { IChannel, IClassEntity, IChannelProperties, IContentDirectoryClass } from "../types";
-import { contentDirClasses } from "./constants";
+import {
+	IChannel,
+	IChannelProperties,
+	IClassEntity,
+	ICategory,
+	IKnownLicense,
+	IUserDefinedLicense,
+	IJoystreamMediaLocation,
+	IHttpMediaLocation,
+	IVideoMedia,
+	IVideo,
+} from "../types";
 
 const debug = Debug("mappings:content-directory");
 
-function _getClassEntity(event: SubstrateEvent): IClassEntity {
+function stringIfyEntityId(event: SubstrateEvent): string {
+	const { 1: entityId } = event.params;
+	return entityId.value as string;
+}
+
+function getClassEntity(event: SubstrateEvent): IClassEntity {
 	const { 0: classId } = event.extrinsic.args;
 	const { 1: entityId } = event.params;
 	return  {
@@ -16,27 +31,62 @@ function _getClassEntity(event: SubstrateEvent): IClassEntity {
 	};
 }
 
-function _getClassById(classId: BN): IContentDirectoryClass {
-	const c = contentDirClasses.find((c) => c.classId === (classId as unknown) as number);
-	if (!c) throw new Error(`Class not found: "${(classId as unknown) as number}"`);
-	return c;
-}
-
 // Get entity id for from entity related substrate event.
-function _getEntityId(event: SubstrateEvent): string {
+function getEntityId(event: SubstrateEvent): string {
 	const { 1: entityId } = event.extrinsic.args;
 	return entityId.value as string;
 }
 
-function _channelEntity(event: SubstrateEvent): IChannel {
+function channelEntity(event: SubstrateEvent): IChannel {
 	debug(`Substrate event: ${JSON.stringify(event)}`);
 
-	const { 1: entityId, 3: newPropertyValues } = event.extrinsic.args;
+	const { 0: actor, 3: newPropertyValues } = event.extrinsic.args;
 	const properties = (newPropertyValues.value as unknown) as IChannelProperties;
 	return {
-		id: entityId.value as string,
+		accountId: Buffer.from(actor.value as string),
 		properties,
 	};
 }
 
-export const decode = { _getClassEntity, _getEntityId, _getClassById, _channelEntity };
+function categoryEntity(event: SubstrateEvent): ICategory {
+	debug(`Substrate event: ${JSON.stringify(event)}`);
+	return (event.extrinsic.args[3].value as unknown) as ICategory;
+}
+
+function knownLicenseEntity(event: SubstrateEvent): IKnownLicense {
+	return (event.extrinsic.args[3].value as unknown) as IKnownLicense;
+}
+
+function userDefinedLicenseEntity(event: SubstrateEvent): IUserDefinedLicense {
+	return (event.extrinsic.args[3].value as unknown) as IUserDefinedLicense;
+}
+
+function joystreamMediaLocationLicenseEntity(event: SubstrateEvent): IJoystreamMediaLocation {
+	return (event.extrinsic.args[3].value as unknown) as IJoystreamMediaLocation;
+}
+
+function httpMediaLocationEntity(event: SubstrateEvent): IHttpMediaLocation {
+	return (event.extrinsic.args[3].value as unknown) as IHttpMediaLocation;
+}
+
+function videoMediaEntity(event: SubstrateEvent): IVideoMedia {
+	return (event.extrinsic.args[3].value as unknown) as IVideoMedia;
+}
+
+function videoEntity(event: SubstrateEvent): IVideo {
+	return (event.extrinsic.args[3].value as unknown) as IVideo;
+}
+
+export const decode = {
+	stringIfyEntityId,
+	channelEntity,
+	categoryEntity,
+	knownLicenseEntity,
+	userDefinedLicenseEntity,
+	joystreamMediaLocationLicenseEntity,
+	httpMediaLocationEntity,
+	videoMediaEntity,
+	videoEntity,
+	getClassEntity,
+	getEntityId
+};
